5e7397908652f1c6923c650a4dbbbe77e6ed1529
iMAC <sergio.pereo@outlook.com>
2022-01-29 04:21:33 -0600

    .

--- scheduler/management/commands/runapscheduler.py
+++ scheduler/management/commands/runapscheduler.py
@@ -7,14 +7,20 @@ from django_apscheduler.jobstores import DjangoJobStore
 from django_apscheduler.models import DjangoJobExecution
 from django_apscheduler import util
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 
 def obtain_nyse_symbols():
+    logger.info("start nyse")
     payload = {"api_token": '61f4b1120141b7.04455674', "fmt": "json"}
     r = requests.get('https://eodhistoricaldata.com/api/exchange-symbol-list/US', params=payload)
     tickers = r.json()
     Symbol.objects.all().delete()
     for ticker in tickers:
+        logger.info(ticker["Code"])
         symbol = Symbol(value=ticker["Code"])
         symbol.save()
 
@@ -31,4 +37,11 @@ class Command(BaseCommand):
       max_instances=1,
       replace_existing=True,
     )
-    scheduler.start()
+    logger.info("Added job 'obtain_nyse_symbols'.")
+    try:
+        logger.info("Starting scheduler...")
+        scheduler.start()
+    except KeyboardInterrupt:
+        logger.info("Stopping scheduler...")
+        scheduler.shutdown()
+        logger.info("Scheduler shut down successfully!")
