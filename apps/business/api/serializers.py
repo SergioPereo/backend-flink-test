dc10d7d638e91ffcc7e56f41ae0fef8847832b4a
iMAC <sergio.pereo@outlook.com>
2022-01-29 02:09:30 -0600

    configuration update

--- apps/business/api/serializers.py
+++ apps/business/api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from apps.business.models import Symbol
 
-class SymbolSerializer(serializers.Serializer)
+class SymbolSerializer(serializers.Serializer):
     class Meta:
         model = Symbol
         fields = '__all__'
--- apps/business/api/urls.py
+++ apps/business/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from apps.business.api.api import CompaniesData
+from apps.business.api.api import CompaniesData, Symbols
 
 urlpatterns = [
-    path('companies/', CompaniesData.as_view(), name='business_data'),
+    path('companies/', CompaniesData.as_view(), name='business_data_companies'),
+    path('symbols/', Symbols.as_view(), name='business_data_symbols')
 ]
